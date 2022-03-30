e8965a240ff1c1dbb69da64c667da5cd13634be2
Briain Keddy <71636045+Briain-Keddy@users.noreply.github.com>
2022-03-30 13:22:06 +0100

    Add files via upload

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,17 @@
 <template>
   <b-col>
     <h2>About Us</h2>
-    
+    <p class="about">
+      This website about films and TV Series, you can find details about each film and TV Series. 
+    </p>
   </b-col>
 </template>
 
 <script>
-
 </script>
 
 <style>
+.about{
+  font-size: 16px;
+}
 </style>
